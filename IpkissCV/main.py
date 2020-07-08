8d8d88bef32975c802f10fd54d8d8e9619f1583c
Alexandre Pereira <a.junior@aluno.ifsp.edu.br>
2020-07-08 17:27:35 -0300

    Comentando Código

--- IpkissCV/main.py
+++ IpkissCV/main.py
@@ -15,10 +15,11 @@ import cv2
 #a saída e uma trava usada para garantir a segurança de threads
 # trocas dos quadros na saída
 saida = None
+
 trava = threading.Lock()
-classificador = open('classificadores/haarcascade_frontalface_default.xml')
 app = Flask(__name__)
 
+#Aqui Video Stream
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
@@ -29,43 +30,51 @@ def index():
 
 def detectar_faces(contador):
 
+    #Variaveis Globais
     global vs, saida, trava
+    #intanciando
     fd = Ipkiss(fps=0.1)
     total = 0
     while True:
+        #frame recebe as imagens captadas
         frame =  vs.read()
+        #imutils renderiza para o tamanho para 400x400px
         frame = imutils.resize(frame, width=400)
+        #cinza recebe uma cópia em escala de cinza da imagem
         cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         if total > contador:
-
+            #a função detectar retorna as coordenadas do(s) rosto(s)
             faces = fd.detectar(cinza)
             for (x, y, largura, altura) in faces:
+                #cv2 utiliza as coordenas para fazer um retangulo, nesse caso vermelho de 2px
                 cv2.rectangle(frame, (x, y), (x + largura, y + altura), (0, 0, 255), 2)
-
+        #detectando ou não, a imagem é atualizada
         fd.atualizar(cinza)
         total += 1
         with trava:
             saida = frame.copy()
-def gerar():
 
+def gerar():
+    #Variavel global
     global saida, trava
 
     while True:
-
         with trava:
             if saida is None:
                 continue
+             #imencode transforma em um buffer
             (flag, imagem_codificada) = cv2.imencode(".jpg", saida)
 
             if not flag:
                 continue
-
+        #e imagem codificada se transforma em um array de bytes
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
             bytearray(imagem_codificada) + b'\r\n')
 
 @app.route("/video_raiz")
 def video_raiz():
+    #quando iniciado, video raiz chama a funcão gerar
     return Response(gerar(), mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == '__main__':
@@ -79,6 +88,7 @@ if __name__ == '__main__':
         help="#frames para construir o fundo")
     args = vars(ap.parse_args())
 
+    #usandos os dados passados
     t = threading.Thread(target=detectar_faces, args=(
         args["frame_count"],))
     t.daemon = True
--- IpkissCV/models/ipkiss.py
+++ IpkissCV/models/ipkiss.py
@@ -1,5 +1,5 @@
-import numpy as np
-import imutils
+#import numpy as np
+#import imutils
 import cv2
 
 class Ipkiss:
@@ -7,15 +7,19 @@ class Ipkiss:
     def __init__(self,fps=0.5):
         self.fps = fps
         self.fundo = None
+        #aqui é chamado o classificador
         self.classificador = cv2.CascadeClassifier("classificadores/haarcascade_frontalface_default.xml")
 
     def atualizar(self, imagem):
         if self.fundo is None:
             self.fundo = imagem.copy().astype("float")
             return
+        #cv2 faz a transição da imagem de fundo para a imagem, dependendo do fps para a velocidade da troca
         cv2.accumulateWeighted(imagem, self.fundo, self.fps)
 
     def detectar(self,imagem_cinza):
+        #a função detectMultiScale, recebe a imagem , a escala e os neightboor que serão comparados para formar o rosto
+        #ela retorna faces que contém os pontos cartesianos de onde começa e termina a face
         faces = self.classificador.detectMultiScale(imagem_cinza,1.3,5)
         return faces
 
